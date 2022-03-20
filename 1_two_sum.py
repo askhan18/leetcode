b92c29bfd16d56aeb8bd1d04951b8be4553298d3
askhan18 <askhan4@dons.usfca.edu>
2022-03-20 15:39:43 -0700

    Adding 1_two_sum.py

---
+++ 1_two_sum.py
@@ -0,0 +1,15 @@
+'''
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for ix, num in enumerate(nums):
+            diff = target - num
+            if diff in seen:
+                return [ix, seen[diff]]
+            else:
+                seen[num] = ix
